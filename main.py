dacbd45e89840e229742fa7f1b4aa0309654fb4d
0xSalim <contenant.matage.0u@icloud.com>
2023-05-09 15:32:24 +0200

    update prompt and add more tools

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import argparse
 import os
+import time
 
 from dotenv import load_dotenv
-from langchain.agents import initialize_agent
+from langchain.agents import AgentType, initialize_agent
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.llms import OpenAI
 
-from tools import clone_tool, list_directory_tool, read_file_tool
+from tools import (
+    git_clone_tool,
+    list_directory_tool,
+    read_file_tool,
+    write_file_tool,
+)
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
@@ -23,16 +29,21 @@ def main():
 
 
 def generate_repo_readme(repo_url: str):
-    llm = OpenAI(api_key=OPENAI_API_KEY, temperature=0.2)
+    llm = OpenAI(api_key=OPENAI_API_KEY, temperature=0)
 
-    tools = [clone_tool, list_directory_tool, read_file_tool]
+    tools = [
+        git_clone_tool,
+        list_directory_tool,
+        read_file_tool,
+        write_file_tool,
+    ]
 
     memory = ConversationBufferWindowMemory(
         memory_key="chat_history", k=3, return_messages=True
     )
 
     conversational_agent = initialize_agent(
-        agent="chat-conversational-react-description",
+        agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
         tools=tools,
         llm=llm,
         verbose=True,
@@ -41,8 +52,13 @@ def generate_repo_readme(repo_url: str):
         memory=memory,
     )
 
-    question = f"""Generate a README file for this repository: {repo_url}. You need to clone it with the appropriate tool.
-    Only output the README.md file content in a Markdown format and nothing else."""
+    question = f"""You need to enhance the README.md file in this repository: {repo_url}.
+    Your task is the following:
+    1. Clone the repository with the appropriate tool. The repository files will directly be cloned in the working directory.
+    1. Read and understand what the repository does, by listing all files in `.`.
+    2. Create a file named `BETTER_README.md`, that describes the project, what it does, and how to use it, in a Markdown format. This file should allow anyone to understand and use the project.
+    Let's work this out in a step by step way to be sure we have the right answer."""
 
     print(question)
     conversational_agent.run(question)
+    time.sleep(100)
--- tools.py
+++ tools.py
@@ -2,22 +2,67 @@ import subprocess
 from tempfile import TemporaryDirectory
 
 from langchain.agents import Tool
-from langchain.tools.file_management import ListDirectoryTool, ReadFileTool
+from langchain.agents.agent_toolkits import FileManagementToolkit
 
 working_directory = TemporaryDirectory()
 
+toolkit = FileManagementToolkit(root_dir=str(working_directory.name))
 
-def clone_repo(inp: str) -> str:
-    subprocess.run(["git", "clone", inp, working_directory.name])
+
+def git_clone_repo(inp: str) -> str:
+    subprocess.run(["git", "clone", inp, f"{working_directory.name}"])
     return "Repository cloned successfully!"
 
 
-clone_tool = Tool(
+git_clone_tool: Tool = Tool(
     name="Clone Repository",
-    func=clone_repo,
+    func=git_clone_repo,
     description="useful for cloning a repository",
 )
 
-read_file_tool = ReadFileTool()
 
-list_directory_tool = ListDirectoryTool()
+def git_checkout_repo(inp: str) -> str:
+    subprocess.run(["git", "checkout", inp, f"{working_directory.name}"])
+    return "Repository checked out successfully!"
+
+
+git_checkout_tool: Tool = Tool(
+    name="Checkout Repository",
+    func=git_checkout_repo,
+    description="useful for checking out a repository",
+)
+
+
+def git_add_repo(inp: str) -> str:
+    subprocess.run(["git", "add", inp, f"{working_directory.name}"])
+    return "Repository added successfully!"
+
+
+git_add_tool: Tool = Tool(
+    name="Add into Repository",
+    func=git_add_repo,
+    description="useful for adding into a repository",
+)
+
+
+def git_commit_repo(inp: str) -> str:
+    subprocess.run(["git", "commit", "-m", inp, f"{working_directory.name}"])
+    return "Repository committed successfully!"
+
+
+git_commit_tool: Tool = Tool(
+    name="Commit into Repository",
+    func=git_commit_repo,
+    description="useful for committing into a repository",
+)
+
+
+(
+    copy_file_tool,
+    delete_file_tool,
+    search_file_tool,
+    move_file_tool,
+    read_file_tool,
+    write_file_tool,
+    list_directory_tool,
+) = toolkit.get_tools()
